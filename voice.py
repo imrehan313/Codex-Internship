d698e1dd61fdbe0f2fe67fed7a7194d25f6f7c4f
Mohammed Rehanuddin <155258288+imrehan313@users.noreply.github.com>
2025-09-27 19:05:45 +0530

    Voice.py won't work
    Voice.py won't work due to internal issues of github such as lacking of Microphone and Speaker Functionalities, you can still execute this script on your local system

--- voice.py
+++ voice.py
@@ -112,24 +112,24 @@ if __name__=="__main__":
  
  execute="max"
  print("I am here to help you")  
- 
-try:
- with sr.Microphone() as src:
-  userMessage=r.listen(src)
-  userMessage=r.recognize_google(userMessage).lower()
-  print(userMessage)
-
-  if "exit" in userMessage :
-    p.speak("Bye Bye")
-    print("Bye Bye")
-    break
-  
-  elif execute in userMessage:
-   p.speak("how can i help you")
-   print("how can i help you")
-   main()
-  else:
-   pass
-except Exception as e:
-      pass
-   
+ while True: 
+    try:
+     with sr.Microphone() as src:
+      userMessage=r.listen(src)
+      userMessage=r.recognize_google(userMessage).lower()
+      print(userMessage)
+    
+      if "exit" in userMessage :
+        p.speak("Bye Bye")
+        print("Bye Bye")
+        break
+      
+      elif execute in userMessage:
+       p.speak("how can i help you")
+       print("how can i help you")
+       main()
+      else:
+       pass
+    except Exception as e:
+          pass
+       
